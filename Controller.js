42d8ead1dafd7c2e544a78100432111ccdd8f216
Mschukin <mshhukin@yandex.ru>
2024-10-25 14:29:45 +0300

    release1

--- Controller.js
+++ Controller.js
@@ -1,10 +1,36 @@
-const fs = require('fs/promises')
-const model = require('./Model')
-const view = require('./View')
+const fs = require('fs/promises');
+const model = require('./Model');
+const view = require('./View');
 
-// class Controller{
+class Controller {
+  constructor(countAnswer = 0) {
+    this.countAnswer = countAnswer;
+  }
+  static async run() {
+    const getTheme = await View.topicQuiz(); // получаю тему от Асели:
+
+    const classMod = new Model(); //создаю новую модель
+    classMod.getCards(getTheme); //получаю массив объектов -[{вопрос:ответ}]
+
+    while (model.nextCards) {//пока метод проверки карт соответствует условию:
+      const ansQues = classMod.questionGet(); //получаю вопрос от модели
+      const answer = await View.answer(ansQues); //вывожу метод (инпут, вопрос, сообщение)
+      const result = classMod.answerCheck(answer); //запускаю проверку ответа от модели
+      let message = '';
+      if (result) {
+        //вывожу сообщение: если верно, прибавляю к countAnswer+1 и сообщение, если нет, ничего не прибавляю, вывожу сообщение
+        this.countAnswer++;
+        message = 'ты молодец';
+      } else {
+        message = 'иди учись';
+      }
+      View.printConsole(message);
+      const finish = ((this.countAnswer / classMod.questionCount()) * 100).toFixed(1);
+      View.printConsole(`Твой результат - ${message}%`); //запускаю метод сообщения вью, и передаю туда сообщение с результатом
+    }
+  }
+}
 
 
-
-
-// }
+Controller.run()
+module.exports = Controller;
