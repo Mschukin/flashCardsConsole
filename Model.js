c339bd1208e083828f8de8d7a6e524324d53b86c
Mukhammad <jonbola1999@gmail.com>
2024-10-25 14:30:51 +0300

    release_1

--- Model.js
+++ Model.js
@@ -1,1 +1,40 @@
 const fs = require('fs');
+
+class Model {
+  constructor() {
+    this.INDEX = 0;
+    this.cards = [];
+  }
+
+  getCards(path) {
+    const result = (this.cards = fs
+      .readFileSync(`./topics/${path}`, 'utf-8')
+      .split('\n\n')).map((card) => {
+      const [question, answer] = card.split('\n');
+      return { question, answer };
+    });
+
+    return result;
+  }
+
+  questionCount() {
+    return this.cards.length;
+  }
+
+  nextCards() {
+    this.INDEX += 1;
+    return this.INDEX <= this.questionCount();
+  }
+
+  questionGet() {
+    return this.cards[this.INDEX - 1].question;
+  }
+
+  answerCheck(answer) {
+    return answer === this.cards[this.INDEX - 1].answer;
+  }
+}
+
+console.log(Model.getCards('nighthawk_flashcard_data.txt'));
+
+module.exports = Model;
